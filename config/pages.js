6297225c3d7b2903ba31c26804068db455c8048e
David Nichols <davenich@gmail.com>
2017-08-31 15:50:26 -0400

    chore scrape for team defense, too

--- config/pages.js
+++ config/pages.js
@@ -6,12 +6,23 @@ const totalOffenseSelector = {
   numyards_total: ['td[data-stat="tot_yds"]'],
 };
 
+const totalDefenseSelector = {
+  name: ['td[data-stat="school_name"] a'],
+  numplays_total: ['td[data-stat="opp_tot_plays"]'],
+  numyards_total: ['td[data-stat="opp_tot_yds"]'],
+};
+
 module.exports = {
   pages: [
     new Page(
-      'total_offense',
+      'team_offense',
       'https://www.sports-reference.com/cfb/years/2017-team-offense.html',
       totalOffenseSelector
+    ),
+    new Page(
+      'team_defense',
+      'https://www.sports-reference.com/cfb/years/2017-team-defense.html',
+      totalDefenseSelector
     )
   ],
 };
